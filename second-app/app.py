8940f4ef93e7b324732545205b7ca37b444add15
Mark Tennyson <marktennyson1@yahoo.com>
2020-08-07 00:30:57 +0530

    Update app.py

--- second-app/app.py
+++ second-app/app.py
@@ -14,5 +14,13 @@ def index(strData):
     return {'inputMsg':strData, 'md5':md5(data).hexdigest(), 'sha256':sha256(data).hexdigest(), 
             'sha224':sha224(data).hexdigest(),'sha1':sha1(data).hexdigest(), 
             'sha512':sha512(data).hexdigest(), 'base58': b58encode(data).decode('utf-8'), 
-            'base64':b64encode(data).decode('utf-8'), 'password_hash':generate_password_hash(strData)}
+            'base64':b64encode(data).decode('utf-8'), 'password_hash':generate_password_hash(strData),
+            'keySecret':key_secret(strData)}
 
+#@app.route('/key-secret/{secretData}')
+def key_secret(secretData):
+    seed = secretData.encode('utf-8')
+    hashed_seed = md5(seed).hexdigest()
+    encoded_seed = b58encode(hashed_seed.encode('utf-8')).decode('utf-8')
+    return encoded_seed
+    #return {'keySecret':encoded_seed}
